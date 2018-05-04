08b00109ad2d04040c19b8ff3dc6562c1444c816
netwok <37182648+netwok@users.noreply.github.com>
2018-05-04 13:14:23 +0100

    Update pgnfix_print.py

--- pgnfix_print.py
+++ pgnfix_print.py
@@ -1,7 +1,7 @@
 def pgnfix_print(text, src='ICC'):
-  sites = {'ICC': 'Internet Chess Club','LIC': 'lichess.com','CDM': 'chess.com'}
-  if src == 'ICC':
-    list1 = text.replace('</td></tr><tr><td>',' ').replace('</td><td class="fbshare-notation">',' ').replace('</td></tr>','').replace('<tr><td>','')
-    print(list1)
-  else:
-    print('Please specify a valid source from one of the following abbreviations:', **sites, sep='\n') 
+    sites = {'ICC': 'Internet Chess Club','LIC': 'lichess.com','CDM': 'chess.com'}
+    if src == 'ICC':
+        list1 = text.replace('</td></tr><tr><td>',' ').replace('</td><td class="fbshare-notation">',' ').replace('</td></tr>','').replace('<tr><td>','')
+        print(list1)
+    else:
+        print('Please specify a valid source from one of the following abbreviations:', **sites, sep='\n') 
